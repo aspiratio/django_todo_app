85ce448b5e5db4d7db22fcdb5f52efbde1aabd3b
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-03-05 19:32:46 +0900

    タスク詳細画面の追加

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from todoapp.views import TaskList
+from todoapp.views import TaskDetail
 
-urlpatterns = [path("", TaskList.as_view())]
+urlpatterns = [
+    path("", TaskList.as_view()),
+    path("task/<int:pk>/", TaskDetail.as_view()),
+]
