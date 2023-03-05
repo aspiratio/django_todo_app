89bcaeefa25d93ac23e0fdaaef0e10a066f241a4
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-03-05 17:22:46 +0900

    間違いの修正

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("/", views.taskList)]
+urlpatterns = [path("", views.taskList)]
--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -1,5 +1,4 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("todoapp.urls"))]
