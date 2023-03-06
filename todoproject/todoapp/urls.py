9d198552934f0e407ac5900f64ae9f5a3beda838
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-03-07 08:36:10 +0900

    タスクの追加と編集機能を追加

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from todoapp.views import TaskList
 from todoapp.views import TaskDetail
+from todoapp.views import TaskCreate
+from todoapp.views import TaskUpdate
 
 urlpatterns = [
-    path("", TaskList.as_view()),
+    path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task/", TaskCreate.as_view(), name="create-task"),
+    path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 from todoapp.models import Task
 
@@ -15,3 +18,15 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"  # ["user", "title", ....] と書くのと同じ意味（全部のフィールドを指定したいときに便利）
+    success_url = reverse_lazy("tasks")
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"  # ["user", "title", ....] と書くのと同じ意味（全部のフィールドを指定したいときに便利）
+    success_url = reverse_lazy("tasks")
