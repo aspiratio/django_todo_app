9bf5cb7a283354d05f1953d1ee3f9890f0cf840e
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-03-07 22:36:00 +0900

    削除機能を追加

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -3,10 +3,12 @@ from todoapp.views import TaskList
 from todoapp.views import TaskDetail
 from todoapp.views import TaskCreate
 from todoapp.views import TaskUpdate
+from todoapp.views import TaskDelete
 
 urlpatterns = [
     path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
     path("create-task/", TaskCreate.as_view(), name="create-task"),
     path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
+    path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from todoapp.models import Task
 
@@ -30,3 +29,13 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = "__all__"  # ["user", "title", ....] と書くのと同じ意味（全部のフィールドを指定したいときに便利）
     success_url = reverse_lazy("tasks")
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__"  # ["user", "title", ....] と書くのと同じ意味（全部のフィールドを指定したいときに便利）
+    success_url = reverse_lazy("tasks")
+    # デフォルトの context_object_name は object (object_list)、またはモデルに設定したクラスを小文字に変えたもの task (task_list)である
+    # https://docs.djangoproject.com/en/4.1/topics/class-based-views/generic-display/
+    # あえて"task"と設定する必要はないが明示的に指定した方がわかりやすいので設定しておく
+    context_object_name = "task"
